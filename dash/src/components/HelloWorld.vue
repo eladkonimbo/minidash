9c40f4babfe89b2541d72a2250d7ffe4d911a58a
eladkonimbo <elad@konimbo.co.il>
2019-11-24 16:27:25 +0200

    setupApp

---
+++ dash/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+  data() { 
+    return {
+      orders:Object
+    } 
+  },
+  mounted: function() {
+    let api = 'https://api.konimbo.co.il/v1/items/?token=26699ce2c104def7accdca7f308036fa2e20bc8e2c01325ece975444ab139d7a&id=782612'
+    this.axios.get(api).then((response) => {
+      this.orders = response
+      console.log(response.data)
+    })
+  },
+  methods: {
+    
+  }
+}
+</script>
